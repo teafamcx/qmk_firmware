732ecb3045b6e673ce729904014ceb662149507b
darasineegamer@gmail.com <darasine>
2023-08-14 01:21:39 +0900

    keychronK2ProをJP配列設定で使うマップ追加

---
+++ keyboards/keychron/k2_pro/ansi/rgb/keymaps/teafamcx/keymap.c
@@ -0,0 +1,111 @@
+/* Copyright 2022 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "keymap_japanese.h"
+
+// clang-format off
+enum layers{
+  MAC_BASE,
+  MAC_FN,
+  WIN_BASE,
+  WIN_FN
+};
+
+/// Windowsキーコードマッピング変更
+/// GRV -> JP版キーコード
+/// EQL -> JP版...
+/// [] -> JP...
+/// バックスラッシュ -> 円
+/// ' -> JP...
+/// Win -> Alt
+/// LALT -> 無変換
+/// RALT -> 変換
+/// Fn + Win -> Win
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[MAC_BASE] = LAYOUT_ansi_84(
+     KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_SNAP,  KC_DEL,   RGB_MOD,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
+     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
+     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
+     KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD,MO(MAC_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+[MAC_FN] = LAYOUT_ansi_84(
+     KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,  RGB_TOG,
+     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+
+[WIN_BASE] = LAYOUT_ansi_84(
+     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   RGB_MOD,
+     JP_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  JP_EQL,   KC_BSPC,            KC_PGUP,
+     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     JP_LBRC,  JP_RBRC,  JP_YEN,            KC_PGDN,
+     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  JP_QUOT,            KC_ENT,             KC_HOME,
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
+     KC_LCTL,  KC_LALT,  JP_MHEN,                                KC_SPC,                                 JP_HENK, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+[WIN_FN] = LAYOUT_ansi_84(
+     KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  RGB_TOG,
+     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_LGUI,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)
+};
+
+/// SHIFT + ` -> ~
+const key_override_t kor_jp_tild = ko_make_basic(MOD_MASK_SHIFT, JP_GRV, JP_TILD);
+/// SHIFT + 2 -> @
+const key_override_t kor_jp_at = ko_make_basic(MOD_MASK_SHIFT, KC_2, JP_AT);
+/// SHIFT + 6 -> JP_CIRC
+const key_override_t kor_jp_circ = ko_make_basic(MOD_MASK_SHIFT, KC_6, JP_CIRC);
+/// SHIFT + 7 -> &
+const key_override_t kor_jp_ampr = ko_make_basic(MOD_MASK_SHIFT, KC_7, JP_AMPR);
+/// SHIFT + 8 -> *
+const key_override_t kor_jp_astr = ko_make_basic(MOD_MASK_SHIFT, KC_8, JP_ASTR);
+/// SHIFT + 9 -> (
+const key_override_t kor_jp_lprn = ko_make_basic(MOD_MASK_SHIFT, KC_9, JP_LPRN);
+/// SHIFT + 0 -> )
+const key_override_t kor_jp_rprn = ko_make_basic(MOD_MASK_SHIFT, KC_0, JP_RPRN);
+/// SHIFT + MINUS:- -> _
+const key_override_t kor_jp_unds = ko_make_basic(MOD_MASK_SHIFT, KC_MINS, JP_UNDS);
+/// SHIFT + EQUAL:= -> PLUS:+
+const key_override_t kor_jp_plus = ko_make_basic(MOD_MASK_SHIFT, JP_EQL, JP_PLUS);
+/// SHIFT + \ -> |
+const key_override_t kor_jp_pipe = ko_make_basic(MOD_MASK_SHIFT, JP_YEN, JP_PIPE);
+/// SHIFT + ; -> :
+const key_override_t kor_jp_coln = ko_make_basic(MOD_MASK_SHIFT, KC_SCLN, JP_COLN);
+/// SHIFT + ' -> "
+const key_override_t kor_jp_dquo = ko_make_basic(MOD_MASK_SHIFT, JP_QUOT, JP_DQUO);
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &kor_jp_tild,
+    &kor_jp_at,
+    &kor_jp_circ,
+    &kor_jp_ampr,
+    &kor_jp_astr,
+    &kor_jp_lprn,
+    &kor_jp_rprn,
+    &kor_jp_unds,
+    &kor_jp_plus,
+    &kor_jp_pipe,
+    &kor_jp_coln,
+    &kor_jp_dquo,
+    NULL // オーバーライドの配列は Null で終了します！
+};
